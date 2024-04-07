31a2212959816efdaa6d8f56ce567a887cfdf82c
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2024-04-07 20:52:01 +0530

    Update firebase.js

--- src/server/firebase.js
+++ src/server/firebase.js
@@ -1,7 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
-const firebaseConfig = "Place your Configurations Here";
+const firebaseConfig = {
+  apiKey: "AIzaSyCufMY0C-6FWeHLDbBr7ALHBhwhRnA1BK4",
+  authDomain: "unicom-af6ce.firebaseapp.com",
+  databaseURL: "https://unicom-af6ce-default-rtdb.firebaseio.com",
+  projectId: "unicom-af6ce",
+  storageBucket: "unicom-af6ce.appspot.com",
+  messagingSenderId: "1074562457013",
+  appId: "1:1074562457013:web:abcd8a275e93d1e17acdfc",
+  measurementId: "G-Y4M8ELM6RR"
+};
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
